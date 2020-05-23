950363e3c477fb5462fa146a9878267fd96fbd12
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-05-24 00:10:08 +0200

    Add div notes to App.js

--- quizlet/src/App.js
+++ quizlet/src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>Navbar</div>
+      <div>SignUpModal</div>
+      <div>LoginModal</div>
+    </div>
+  );
 }
 
 export default App;

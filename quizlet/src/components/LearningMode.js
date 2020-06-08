15d1d2229ea9ff2cca63270e48d72b0bcee1b3e2
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-06-08 17:51:41 +0200

    Style radio buttons

--- quizlet/src/components/LearningMode.js
+++ quizlet/src/components/LearningMode.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { StyledTitle } from "./elements/LearningModeElements";
+import {
+  Container,
+  StyledTitle,
+  StyledLi,
+  StyledUl,
+  StyledInput,
+  StyledLabel,
+  Check,
+} from "./elements/LearningModeElements";
 
 const LearningMode = (props) => {
   const urlString = props.location.pathname;
+  const difficulty = ["easy", "medium", "hard"];
+  const numbers = [10, 30, 50];
 
   const getTopicName = (string) => {
     let n = string.lastIndexOf("/");
@@ -21,9 +31,77 @@ const LearningMode = (props) => {
     return array.join(" ");
   };
 
+  const difficultyList = difficulty.map((item) => {
+    return <option value={item}>{item}</option>;
+  });
+
+  const numberOfquestions = numbers.map((item) => {
+    return <option value={item}>{item}</option>;
+  });
+
   return (
     <div>
       <StyledTitle>{title(getTopicName(urlString))}</StyledTitle>
+      <form>
+        <Container>
+          <h2>Select Difficulty:</h2>
+          <StyledUl>
+            <StyledLi>
+              <StyledInput type="radio" name="selector" />
+              <StyledLabel>Easy</StyledLabel>
+              <Check></Check>
+            </StyledLi>
+            <StyledLi>
+              <StyledInput type="radio" name="selector" />
+              <StyledLabel>Medium</StyledLabel>
+
+              <Check>
+                <div className="inside"></div>
+              </Check>
+            </StyledLi>
+            <StyledLi>
+              <StyledInput type="radio" name="selector" />
+              <StyledLabel>Hard</StyledLabel>
+
+              <Check>
+                <div className="inside"></div>
+              </Check>
+            </StyledLi>
+          </StyledUl>
+        </Container>
+      </form>
+      <form>
+        <div className="container">
+          <h2>Your favorite thing in the world:</h2>
+
+          <ul>
+            <li>
+              <input type="radio" id="f-option" name="selector" />
+              <label>Pizza</label>
+
+              <div className="check"></div>
+            </li>
+
+            <li>
+              <input type="radio" id="s-option" name="selector" />
+              <label>Bacon</label>
+
+              <div className="check">
+                <div className="inside"></div>
+              </div>
+            </li>
+
+            <li>
+              <input type="radio" id="t-option" name="selector" />
+              <label>Cats</label>
+
+              <div className="check">
+                <div className="inside"></div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </form>
     </div>
   );
 };
--- quizlet/src/components/elements/LearningModeElements.js
+++ quizlet/src/components/elements/LearningModeElements.js
@@ -9,3 +9,87 @@ export const StyledTitle = styled.h1`
   letter-spacing: 1.5px;
   color: #66fcf1;
 `;
+
+export const Container = styled.div`
+  display: block;
+  position: relative;
+  margin: 40px auto;
+  height: auto;
+  width: auto;
+  padding: 20px;
+`;
+
+export const StyledUl = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  overflow: auto;
+`;
+
+export const StyledLi = styled.li`
+  color: #aaaaaa;
+  display: block;
+  position: relative;
+  float: left;
+  width: 100%;
+  height: 100px;
+  border-bottom: 1px solid #333;
+`;
+
+export const StyledInput = styled.input`
+  position: absolute;
+  visibility: hidden;
+`;
+
+export const StyledLabel = styled.label`
+  display: block;
+  position: relative;
+  font-weight: 300;
+  font-size: 1.35em;
+  padding: 25px 25px 25px 80px;
+  margin: 10px auto;
+  height: 30px;
+  z-index: 9;
+  cursor: pointer;
+  transition: all 0.25s linear;
+  &:hover {
+    color: #ffffff;
+  }
+  &:checked {
+    color: #0dff92;
+  }
+`;
+
+export const Check = styled.div`
+  display: block;
+  position: absolute;
+  border: 5px solid #aaaaaa;
+  border-radius: 100%;
+  height: 25px;
+  width: 25px;
+  top: 30px;
+  left: 20px;
+  z-index: 5;
+  transition: border 0.25s linear;
+  -webkit-transition: border 0.25s linear;
+  &:hover {
+    border: 5px solid #ffffff;
+  }
+  &::before {
+    display: block;
+    position: absolute;
+    content: "";
+    border-radius: 100%;
+    height: 15px;
+    width: 15px;
+    top: 5px;
+    left: 5px;
+    margin: auto;
+    &:checked {
+      background: #0dff92;
+    }
+  }
+  &:checked {
+    border: 5px solid #0dff92;
+  }
+`;
